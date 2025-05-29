1f1991b17dd4a40987db8533bb89ccfdeb6df455
Godisouleymane <ssabiougodi@gmail.com>
2025-05-30 00:24:02 +0100

    premier commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,55 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
     int n1, n2;
-    scanf("%d", &n1);
-    int *set1 = malloc(n1 * sizeof(int));
-    if (set1 == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        return 1;
-    }
-
-    for (int i = 0; i < n1; i++) {
-        scanf("%d", &set1[i]);
-    }
-
-    scanf("%d", &n2);
-    int *set2 = malloc(n2 * sizeof(int));
-    if (set2 == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        free(set1);
-        return 1;
-    }
-
-    for (int i = 0; i < n2; i++) {
-        scanf("%d", &set2[i]);
-    }
-
-    printf("Intersection :");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
-            if (set1[i] == set2[j]) {
-                printf(" %d", set1[i]);
-                break;
-            }
-        }
-    }
-    printf("\n");
-
-    free(set1);
-    free(set2);
-
-    return 0;
-}
-
-
-/* 
-#include <stdio.h>
-
-int main() {
-    int n1, n2;
-
     scanf("%d", &n1);
     int set1[n1];
     for (int i = 0; i < n1; i++) {
@@ -62,17 +14,38 @@ int main() {
         scanf("%d", &set2[i]);
     }
 
-    printf("Intersection :");
+    printf("Intersection:");
+    int first = 1; // pour gérer l’espace
+
     for (int i = 0; i < n1; i++) {
+        // Vérifier si set1[i] est dans set2
+        int found_in_set2 = 0;
         for (int j = 0; j < n2; j++) {
             if (set1[i] == set2[j]) {
+                found_in_set2 = 1;
+                break;
+            }
+        }
+
+        // Vérifier si déjà affiché
+        int already_printed = 0;
+        for (int k = 0; k < i; k++) {
+            if (set1[i] == set1[k]) {
+                already_printed = 1;
+                break;
+            }
+        }
+
+        if (found_in_set2 && !already_printed) {
+            if (first) {
                 printf(" %d", set1[i]);
-                break; // pour éviter les doublons
+                first = 0;
+            } else {
+                printf(" %d", set1[i]);
             }
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
-    */
