1ecc5c4482d26e5211313dfa549568bddd618461
Godisouleymane <ssabiougodi@gmail.com>
2025-05-30 00:07:29 +0100

    premier commit

--- array_intersection.c
+++ array_intersection.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n1, n2;
+    scanf("%d", &n1);
+    int *set1 = malloc(n1 * sizeof(int));
+    if (set1 == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", &set1[i]);
+    }
+
+    scanf("%d", &n2);
+    int *set2 = malloc(n2 * sizeof(int));
+    if (set2 == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        free(set1);
+        return 1;
+    }
+
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &set2[i]);
+    }
+
+    printf("Intersection :");
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (set1[i] == set2[j]) {
+                printf(" %d", set1[i]);
+                break;
+            }
+        }
+    }
+    printf("\n");
+
+    free(set1);
+    free(set2);
+
+    return 0;
+}
+
+
+/* 
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+
     scanf("%d", &n1);
     int set1[n1];
     for (int i = 0; i < n1; i++) {
@@ -27,3 +75,4 @@ int main() {
 
     return 0;
 }
+    */
