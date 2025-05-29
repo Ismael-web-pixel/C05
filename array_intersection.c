f276f1da0f4ee2d2ee36c6e2cb9612089f2b6fd9
Godisouleymane <ssabiougodi@gmail.com>
2025-05-30 00:31:52 +0100

    premier commit

--- array_intersection.c
+++ array_intersection.c
@@ -4,44 +4,30 @@ int main() {
     int n1, n2;
     scanf("%d", &n1);
     int set1[n1];
-    for (int i = 0; i < n1; i++) {
-        scanf("%d", &set1[i]);
-    }
+    for (int i = 0; i < n1; i++) scanf("%d", &set1[i]);
 
     scanf("%d", &n2);
     int set2[n2];
-    for (int i = 0; i < n2; i++) {
-        scanf("%d", &set2[i]);
-    }
+    for (int i = 0; i < n2; i++) scanf("%d", &set2[i]);
 
     printf("Intersection:");
-    int first = 1; // pour gérer l’espace
+    int first = 1;
 
     for (int i = 0; i < n1; i++) {
-        // Vérifier si set1[i] est dans set2
-        int found_in_set2 = 0;
-        for (int j = 0; j < n2; j++) {
-            if (set1[i] == set2[j]) {
-                found_in_set2 = 1;
-                break;
-            }
-        }
-
-        // Vérifier si déjà affiché
-        int already_printed = 0;
+        int duplicate = 0;
         for (int k = 0; k < i; k++) {
             if (set1[i] == set1[k]) {
-                already_printed = 1;
+                duplicate = 1;
                 break;
             }
         }
+        if (duplicate) continue;
 
-        if (found_in_set2 && !already_printed) {
-            if (first) {
-                printf(" %d", set1[i]);
+        for (int j = 0; j < n2; j++) {
+            if (set1[i] == set2[j]) {
+                printf("%s%d", first ? " " : " ", set1[i]);
                 first = 0;
-            } else {
-                printf(" %d", set1[i]);
+                break;
             }
         }
     }
